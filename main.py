a90f981c60d46421cefb8fb32165330d5da43a11
Minato <70564138+Theheirofzeus@users.noreply.github.com>
2022-01-16 19:09:48 +0530

    ok so bot

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from ssl import ALERT_DESCRIPTION_HANDSHAKE_FAILURE
 from pyrogram import Client, filters
 import os 
-session_name = os.environ.get("SESSION")
+bot_token = os.environ.get("TOKEN")
 api_id = os.environ.get("API_ID")
 api_hash =os.environ.get("API_HASH")
-app = Client(session_name, api_id, api_hash,"pyro")
+app = Client(bot_token, api_id, api_hash,"pyro")
 
 
 @app.on_message(filters.private|filters.command("start"))
