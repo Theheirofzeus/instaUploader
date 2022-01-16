0bd088423a289d837ed07b3d79b816695f3a02da
Minato <70564138+Theheirofzeus@users.noreply.github.com>
2022-01-16 20:25:04 +0530

    Create __main__.py

---
+++ Bot/__main__.py
@@ -0,0 +1,9 @@
+from pyrogram import Client, filters
+from Bot import app
+
+@app.on_message(filters.private|filters.command("start"))
+async def start(client, message):
+    await message.reply("Hello imma downloader")
+
+
+app.run()
