2f7faf3e667169183b71915d6d2b6f4967a11a09
Minato <70564138+Theheirofzeus@users.noreply.github.com>
2022-01-16 20:31:41 +0530

    Update __init__.py

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -4,5 +4,5 @@ import os
 bot_token = os.environ.get("TOKEN")
 api_id = os.environ.get("API_ID")
 api_hash =os.environ.get("API_HASH")
-app = Client(bot_token, api_id, api_hash,"pyro")
+app = Client(session_name="pyro", bot_token=bot_token, api_id=api_id, api_hash=api_hash)
 
