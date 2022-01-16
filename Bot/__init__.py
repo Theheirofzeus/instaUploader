e9e93bdafa1fa28b5699cc61efb511c7370bf8c5
Minato <70564138+Theheirofzeus@users.noreply.github.com>
2022-01-16 20:25:19 +0530

    Update __init__.py

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -6,10 +6,3 @@ api_id = os.environ.get("API_ID")
 api_hash =os.environ.get("API_HASH")
 app = Client(bot_token, api_id, api_hash,"pyro")
 
-
-@app.on_message(filters.private|filters.command("start"))
-async def start(client, message):
-    await message.reply("Hello imma downloader")
-
-
-app.run()
