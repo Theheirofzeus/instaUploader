9e9fd1a83e8eb9c1dc69b8221bc59d4c34c6a7b3
Minato <70564138+Theheirofzeus@users.noreply.github.com>
2022-01-16 20:40:36 +0530

    Update __main__.py

--- Bot/__main__.py
+++ Bot/__main__.py
@@ -1,9 +1,10 @@
 from pyrogram import filters
 from Bot import app
+from .insta import 
 
-@app.on_message(filters.private|filters.command("start"))
 async def start(client, message):
     await message.reply("Hello imma downloader")
 
-
+app.add_handler(MessageHandler(filters.command("start")))
+app.add_handler(MessageHandler(filters.command("start")))
 app.run()
